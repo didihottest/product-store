a8ed12afee6bf54695f83b9ee2f556f16ebe13d6
didimajdi <didimajdi@femaledaily.com>
2025-02-23 23:11:28 +0700

    feat: add dto validation

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { existsSync, mkdirSync } from 'fs';
 import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const uploadDir = join(process.cwd(), 'uploads');
   app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
